3cb9ec0dc3870e115de6864727f5bd39a33fdf60
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-04-25 21:15:04 +0800

    Initial commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,8 +9,8 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: true,
-        sameSite: 'none'
+        sameSite: 'None',
+        secure: true
     };
     res.status(statusCode).cookie("token", token, options).json({ success: true, token, user });
 }
