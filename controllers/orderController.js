3677d4dc77278ba9eb418b90e99998973dedad88
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-05-15 03:48:21 +0800

    Rollback

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -60,205 +60,23 @@ const handlePayMongo = async (orderItemsDetails, temporaryLink) => {
     }
 };
 
-// exports.newOrder = async (req, res, next) => {
-//     try {
-//         const validationErrors = [];
-//         const user = await UserModel.findById(req.body.user);
-
-//         if (!user) {
-//             return next(new ErrorHandler("User not found", 404));
-//         }
-
-//         const orderItemsDetails = [];
-
-//         const orderItemsIds = await Promise.all(
-//             req.body.orderItems.map(async (orderItem) => {
-//                 const product = await ProductModel.findById(orderItem.product).populate("brand");
-
-//                 if (!product) {
-//                     validationErrors.push(`Product with ID ${orderItem.id} not found.`);
-//                     return null;
-//                 }
-
-//                 if (product.stock <= 0) {
-//                     validationErrors.push(`Out of stock of product ${product.name}.`);
-//                     return null;
-//                 }
-
-//                 if (product.stock < orderItem.quantity) {
-//                     validationErrors.push(`Not enough stock of product ${product.name}.`);
-//                     return null;
-//                 }
-
-//                 // Check if payment method is Cash On Delivery
-//                 if (req.body.paymentMethod === "Cash On Delivery") {
-//                     // Log the stock change
-//                     const stockChange = -orderItem.quantity;
-
-//                     // Update the stockLogs
-//                     product.stockLogs.push({
-//                         name: product.name,
-//                         brand: product.brand.name,
-//                         quantity: stockChange,
-//                         status: "Sold",
-//                         by: `${user.firstname} - ${user.role}`,
-//                     });
-
-//                     if (product) {
-//                         product.stock -= orderItem.quantity;
-//                         await product.save();
-//                     }
-//                 }
-
-//                 let newOrderItem = new OrderItemsModel({
-//                     quantity: orderItem.quantity,
-//                     product: orderItem.product,
-//                 });
-
-//                 orderItemsDetails.push({
-//                     productName: product.name,
-//                     quantity: orderItem.quantity,
-//                     price: product.price,
-//                 });
-
-//                 newOrderItem = await newOrderItem.save();
-//                 return newOrderItem._id;
-//             })
-//         );
-
-//         // Check if there were validation errors
-//         if (validationErrors.length > 0) {
-//             console.log(validationErrors);
-//             return res.status(400).send(validationErrors.join("\n"));
-//         }
-
-//         const initialOrderStatus = {
-//             // Add the initial status when creating the order
-//             status: req.body.paymentMethod === "GCash" ? "TOPAY" : "Pending",
-//             timestamp: new Date(),
-//             message:
-//                 req.body.paymentMethod === "GCash"
-//                     ? "Order placed successfully. Proceed to payment using GCash."
-//                     : "Order placed successfully.",
-//         };
-
-//         let order = new OrderModel({
-//             orderItems: orderItemsIds,
-//             user: req.body.user,
-//             fullname: req.body.fullname,
-//             phone: req.body.phone,
-//             region: req.body.region,
-//             province: req.body.province,
-//             city: req.body.city,
-//             barangay: req.body.barangay,
-//             postalcode: req.body.postalcode,
-//             address: req.body.address,
-//             orderStatus: [initialOrderStatus], // Include the initial order status
-//             totalPrice: req.body.totalPrice,
-//             customerUser: req.body.customerUser,
-//             employeeUser: req.body.employeeUser,
-//             dateOrdered: req.body.dateOrdered,
-//             dateReceived: req.body.dateReceived,
-//             paymentMethod: req.body.paymentMethod,
-//         });
-
-//         order = await order.save();
-
-//         const orderObject = order.toObject();
-
-//         console.log(orderItemsDetails, "this is order");
-
-//         const paymongoToken = await new PaymongoToken({
-//             orderId: order._id,
-//             token: crypto.randomBytes(32).toString("hex"),
-//             verificationTokenExpire: new Date(Date.now() + 2 * 60 * 1000),
-//         }).save();
-
-//         // HTML content for the email
-//         let emailContent = `
-//     <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 15px; justify-content: center; align-items: center; height: 40vh;">
-//     <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
-//         <h1 style="font-size: 24px; color: #333; margin-bottom: 20px;">Order Details</h1>
-//         <p style="font-size: 16px; color: #555;">Hello ${user.firstname},</p>
-//         <p style="font-size: 16px; color: #555;">Thank you for your order with <strong>teamPOOR - Motorcycle Parts & Services</strong>.</p>
-        
-//         <!-- Order items table -->
-//         <table style="width: 100%; margin-top: 20px; border-collapse: collapse;">
-//             <thead>
-//                 <tr>
-//                     <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Product Name</th>
-//                     <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Quantity</th>
-//                     <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Price</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 ${orderItemsDetails.map(item => `
-//                     <tr>
-//                         <td style="padding: 10px; border: 1px solid #ddd;">${item.productName}</td>
-//                         <td style="padding: 10px; border: 1px solid #ddd;">${item.quantity}</td>
-//                         <td style="padding: 10px; border: 1px solid #ddd;">${item.price}</td>
-//                     </tr>
-//                 `).join('')}
-//             </tbody>
-//         </table>
-//         <!-- End of order items table -->
-        
-//         <p style="font-size: 16px; color: #555; margin-top: 20px;">If you have any questions or concerns about your order, please feel free to contact us.</p>
-//         <p style="font-size: 16px; color: #555;">Best regards,<br>teamPOOR - Motorcycle Parts & Services</p>
-//     </div>
-// </div>
-
-//     `;
-
-//         await sendtoEmail(
-//             user.email,
-//             "teamPOOR - Order Confirmation",
-//             emailContent,
-//             true // Set the last parameter to true to indicate HTML content
-//         );
-
-//         if (req.body.paymentMethod === "GCash") {
-//             const temporaryLink = `${process.env.FRONTEND_URL}/paymongo-gcash/${paymongoToken.token}/${order._id}`;
-//             console.log();
-
-//             const checkoutUrl = await handlePayMongo(
-//                 orderItemsDetails,
-//                 temporaryLink
-//             );
-
-//             console.log(checkoutUrl, "checkout");
-
-//             return res.json({ checkoutUrl });
-//         }
-
-//         return res.send(orderObject);
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).send("Internal Server Error");
-//     }
-// };
-
 exports.newOrder = async (req, res, next) => {
-    const session = await mongoose.startSession();
-    session.startTransaction();
-
     try {
         const validationErrors = [];
-        const user = await UserModel.findById(req.body.user).session(session);
+        const user = await UserModel.findById(req.body.user);
 
         if (!user) {
-            await session.abortTransaction();
-            session.endSession();
             return next(new ErrorHandler("User not found", 404));
         }
 
         const orderItemsDetails = [];
+
         const orderItemsIds = await Promise.all(
             req.body.orderItems.map(async (orderItem) => {
-                const product = await ProductModel.findById(orderItem.product).populate("brand").session(session);
+                const product = await ProductModel.findById(orderItem.product).populate("brand");
 
                 if (!product) {
-                    validationErrors.push(`Product with ID ${orderItem.product} not found.`);
+                    validationErrors.push(`Product with ID ${orderItem.id} not found.`);
                     return null;
                 }
 
@@ -272,9 +90,12 @@ exports.newOrder = async (req, res, next) => {
                     return null;
                 }
 
+                // Check if payment method is Cash On Delivery
                 if (req.body.paymentMethod === "Cash On Delivery") {
+                    // Log the stock change
                     const stockChange = -orderItem.quantity;
 
+                    // Update the stockLogs
                     product.stockLogs.push({
                         name: product.name,
                         brand: product.brand.name,
@@ -283,8 +104,10 @@ exports.newOrder = async (req, res, next) => {
                         by: `${user.firstname} - ${user.role}`,
                     });
 
-                    product.stock -= orderItem.quantity;
-                    await product.save({ session });
+                    if (product) {
+                        product.stock -= orderItem.quantity;
+                        await product.save();
+                    }
                 }
 
                 let newOrderItem = new OrderItemsModel({
@@ -298,19 +121,19 @@ exports.newOrder = async (req, res, next) => {
                     price: product.price,
                 });
 
-                newOrderItem = await newOrderItem.save({ session });
+                newOrderItem = await newOrderItem.save();
                 return newOrderItem._id;
             })
         );
 
+        // Check if there were validation errors
         if (validationErrors.length > 0) {
-            await session.abortTransaction();
-            session.endSession();
             console.log(validationErrors);
             return res.status(400).send(validationErrors.join("\n"));
         }
 
         const initialOrderStatus = {
+            // Add the initial status when creating the order
             status: req.body.paymentMethod === "GCash" ? "TOPAY" : "Pending",
             timestamp: new Date(),
             message:
@@ -330,7 +153,7 @@ exports.newOrder = async (req, res, next) => {
             barangay: req.body.barangay,
             postalcode: req.body.postalcode,
             address: req.body.address,
-            orderStatus: [initialOrderStatus],
+            orderStatus: [initialOrderStatus], // Include the initial order status
             totalPrice: req.body.totalPrice,
             customerUser: req.body.customerUser,
             employeeUser: req.body.employeeUser,
@@ -339,78 +162,83 @@ exports.newOrder = async (req, res, next) => {
             paymentMethod: req.body.paymentMethod,
         });
 
-        order = await order.save({ session });
+        order = await order.save();
 
         const orderObject = order.toObject();
+
+        console.log(orderItemsDetails, "this is order");
 
         const paymongoToken = await new PaymongoToken({
             orderId: order._id,
             token: crypto.randomBytes(32).toString("hex"),
             verificationTokenExpire: new Date(Date.now() + 2 * 60 * 1000),
-        }).save({ session });
+        }).save();
 
+        // HTML content for the email
         let emailContent = `
-        <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 15px; justify-content: center; align-items: center; height: 40vh;">
-            <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
-                <h1 style="font-size: 24px; color: #333; margin-bottom: 20px;">Order Details</h1>
-                <p style="font-size: 16px; color: #555;">Hello ${user.firstname},</p>
-                <p style="font-size: 16px; color: #555;">Thank you for your order with <strong>teamPOOR - Motorcycle Parts & Services</strong>.</p>
-                
-                <table style="width: 100%; margin-top: 20px; border-collapse: collapse;">
-                    <thead>
-                        <tr>
-                            <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Product Name</th>
-                            <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Quantity</th>
-                            <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        ${orderItemsDetails.map(item => `
-                            <tr>
-                                <td style="padding: 10px; border: 1px solid #ddd;">${item.productName}</td>
-                                <td style="padding: 10px; border: 1px solid #ddd;">${item.quantity}</td>
-                                <td style="padding: 10px; border: 1px solid #ddd;">${item.price}</td>
-                            </tr>
-                        `).join('')}
-                    </tbody>
-                </table>
-                
-                <p style="font-size: 16px; color: #555; margin-top: 20px;">If you have any questions or concerns about your order, please feel free to contact us.</p>
-                <p style="font-size: 16px; color: #555;">Best regards,<br>teamPOOR - Motorcycle Parts & Services</p>
-            </div>
-        </div>
-        `;
+    <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 15px; justify-content: center; align-items: center; height: 40vh;">
+    <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
+        <h1 style="font-size: 24px; color: #333; margin-bottom: 20px;">Order Details</h1>
+        <p style="font-size: 16px; color: #555;">Hello ${user.firstname},</p>
+        <p style="font-size: 16px; color: #555;">Thank you for your order with <strong>teamPOOR - Motorcycle Parts & Services</strong>.</p>
+        
+        <!-- Order items table -->
+        <table style="width: 100%; margin-top: 20px; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Product Name</th>
+                    <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Quantity</th>
+                    <th style="padding: 10px; background-color: #f2f2f2; border: 1px solid #ddd;">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${orderItemsDetails.map(item => `
+                    <tr>
+                        <td style="padding: 10px; border: 1px solid #ddd;">${item.productName}</td>
+                        <td style="padding: 10px; border: 1px solid #ddd;">${item.quantity}</td>
+                        <td style="padding: 10px; border: 1px solid #ddd;">${item.price}</td>
+                    </tr>
+                `).join('')}
+            </tbody>
+        </table>
+        <!-- End of order items table -->
+        
+        <p style="font-size: 16px; color: #555; margin-top: 20px;">If you have any questions or concerns about your order, please feel free to contact us.</p>
+        <p style="font-size: 16px; color: #555;">Best regards,<br>teamPOOR - Motorcycle Parts & Services</p>
+    </div>
+</div>
+
+    `;
 
         await sendtoEmail(
             user.email,
             "teamPOOR - Order Confirmation",
             emailContent,
-            true
+            true // Set the last parameter to true to indicate HTML content
         );
 
         if (req.body.paymentMethod === "GCash") {
             const temporaryLink = `${process.env.FRONTEND_URL}/paymongo-gcash/${paymongoToken.token}/${order._id}`;
+            console.log();
 
             const checkoutUrl = await handlePayMongo(
                 orderItemsDetails,
                 temporaryLink
             );
 
-            await session.commitTransaction();
-            session.endSession();
+            console.log(checkoutUrl, "checkout");
+
             return res.json({ checkoutUrl });
         }
 
-        await session.commitTransaction();
-        session.endSession();
         return res.send(orderObject);
     } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
         console.error(error);
         return res.status(500).send("Internal Server Error");
     }
 };
+
+
 exports.PaymentOrder = async (req, res, next) => {
     try {
         const order = await OrderModel.findById(req.params.id);
