0872f89a24b3ec18dd13add65267215467fc73f1
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-05-14 14:43:55 +0800

    instead of fieldSize change to fileSize

--- utils/multer.js
+++ utils/multer.js
@@ -1,9 +1,27 @@
+// const multer = require("multer");
+// const path = require("path");
+// const ErrorHandler = require("./errorHandler");
+
+// module.exports = multer({
+//     limits: { fieldSize: 10 * 1024 * 1024 },
+//     storage: multer.diskStorage({}),
+//     fileFilter: (req, file, cb) => {
+//         let ext = path.extname(file.originalname).toLowerCase();
+
+//         if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+//             cb(new ErrorHandler("Unsupported file type!"), false);
+//             return;
+//         }
+//         cb(null, true);
+//     },
+// });
+
 const multer = require("multer");
 const path = require("path");
 const ErrorHandler = require("./errorHandler");
 
 module.exports = multer({
-    limits: { fieldSize: 10 * 1024 * 1024 },
+    limits: { fileSize: 10 * 1024 * 1024 }, // Use fileSize instead of fieldSize
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
         let ext = path.extname(file.originalname).toLowerCase();
